937677a1e61b46d6feb369cd8f106ba6c67fc421
koy6654 <koy410994@gmail.com>
2024-02-08 00:59:33 +0900

    Add docker-compose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // 마이크로서비스 추가
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092', 'localhost:9093'],
+        },
+        consumer: {
+          groupId: 'mung-group-0',
+        },
+      },
+    },
+  );
+
+  // 마이크로서비스 실행
+  await app.listen();
 }
+
 bootstrap();
