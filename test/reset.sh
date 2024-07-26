707d80832c19b2e38ea42b9a9b35b62cac3f6395
Ohyoung Kwon <ohyoung.kwon@probit.com>
2024-07-26 13:31:34 +0900

    Change reset.sh

--- test/reset.sh
+++ test/reset.sh
@@ -2,6 +2,5 @@
 
 docker stop Kafka00Container Kafka01Container Kafka02Container KafkaWebUiContainer ; \
 docker rm Kafka00Container Kafka01Container Kafka02Container KafkaWebUiContainer ; \
-docker-compose up -d
-# docker-compose up -d && \
-# nest start --watch
+docker-compose up -d && \
+nest start --watch
